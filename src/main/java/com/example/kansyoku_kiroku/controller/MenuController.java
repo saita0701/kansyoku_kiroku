3f9544282d43535d88cde01eba35eeab336751dc
saita0701 <yuya970701@gmail.com>
2021-03-18 12:32:22 +0900

    jsとコントローラーの処理を修正

--- src/main/java/com/example/kansyoku_kiroku/controller/MenuController.java
+++ src/main/java/com/example/kansyoku_kiroku/controller/MenuController.java
@@ -32,7 +32,7 @@ public class MenuController {
 	
 	@RequestMapping("/insert")
 	@ResponseBody
-	public void registerMenu(@RequestBody MenuForm f) {
+	public String registerMenu(@RequestBody MenuForm f) {
 		
 		if(loginSession.getUserId() > 0) {
 			Menu menu = new Menu(f);
@@ -43,6 +43,7 @@ public class MenuController {
 			
 			menuMapper.insertMenu(menu);
 		}
+		return gson.toJson(f);
 	}
 	
 	@RequestMapping("/delete")
